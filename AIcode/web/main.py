48da56f8dfda32d58595922ad19e3645f44ddc6c
이효원 <129304827+ehyowon@users.noreply.github.com>
2024-05-31 11:13:07 +0900

    에러 발생화면 + css적용 문제 해결

--- AIcode/web/main.py
+++ AIcode/web/main.py
@@ -1,10 +1,9 @@
-import sys
+from flask import Flask, request, render_template, send_from_directory, url_for
 import os
 import re
-from flask import Flask, request, render_template, send_from_directory, url_for
+import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
-
 from personal_color_analysis import personal_color
 
 app = Flask(__name__)
@@ -18,27 +17,31 @@ def upload_file():
         file = request.files['file']
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(file_path)
-        full_result = personal_color.analysis(file_path)
         
-        match = re.search(r'퍼스널 컬러는 (.+)', full_result)
-        if match:
-            result = match.group(1)
-        else:
-            result = "결과를 찾을 수 없습니다."
+        try:
+            full_result = personal_color.analysis(file_path)
+            match = re.search(r'퍼스널 컬러는 (.+)', full_result)
+            if match:
+                result = match.group(1)
+            else:
+                result = "결과를 찾을 수 없습니다."
 
-        if '봄웜톤(spring)' in result:
-            css_file = 'spring.css'
-        elif '여름쿨톤(summer)' in result:
-            css_file = 'summer.css'
-        elif '가을웜톤(fall)' in result:
-            css_file = 'fall.css'
-        elif '겨울쿨톤(winter)' in result:
-            css_file = 'winter.css'
-        else:
+            if '봄웜톤(spring)' in result:
+                css_file = 'spring.css'
+            elif '여름쿨톤(summer)' in result:
+                css_file = 'summer.css'
+            elif '가을웜톤(fall)' in result:
+                css_file = 'fall.css'
+            elif '겨울쿨톤(winter)' in result:
+                css_file = 'winter.css'
+            else:
+                css_file = ''
+        except ValueError as e:
+            result = str(e)
             css_file = ''
 
         img_url = url_for('uploaded_file', filename=file.filename)
-        css_url = url_for('static', filename=f'css/{css_file}')
+        css_url = url_for('static', filename=f'css/{css_file}') if css_file else ''
         return render_template('result.html', result=result, img_url=img_url, css_url=css_url)
     return render_template('web.html')
 
@@ -56,3 +59,4 @@ def develop():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
